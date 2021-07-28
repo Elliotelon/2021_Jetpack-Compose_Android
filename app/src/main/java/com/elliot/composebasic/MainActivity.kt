7f188ca7b846f93192ee74894d4f434b8681c2d2
kimminkyu <kmk50009@naver.com>
2021-07-28 18:12:17 +0900

    Jetpack Compose Row, Column

--- app/src/main/java/com/elliot/composebasic/MainActivity.kt
+++ app/src/main/java/com/elliot/composebasic/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.elliot.composebasic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.elliot.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,18 +24,83 @@ class MainActivity : ComponentActivity() {
             ComposeBasicTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting("안드로이드.")
                 }
             }
         }
     }
 }
 
+// 뷰
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    //Scaffold 안에서 작업을 해줌
+    Scaffold(
+        //TopBar
+        topBar ={
+        TopAppBar(title = { Text("Elliot Android") },backgroundColor = Color(0xfff25287)) },
+        //FloatingActionButton 위치 아래 끝으로
+        floatingActionButtonPosition = FabPosition.End,
+        //FloatingActionButton
+        floatingActionButton = { FloatingActionButton(onClick = {}){
+            Text("클릭")
+        } },
+    ) {
+        //Text(text = "안녕하세요 $name!")
+        MyComposaableView()
+    }
+
 }
 
+@Composable
+fun MyRowItem(){
+    Log.d("TAG", "MyRowItem : ")
+    // Like Horizontal LinearLayout
+    Row(
+        //Padding, Background
+        Modifier
+            //Row Padding
+            .padding(10.dp)
+            //Row 배경색
+            .background(Color(0xffeaffd0))
+            //가로 꽉 채우기
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        //TextView
+        Text(text = "안녕하세요1",
+            Modifier
+                .padding(all = 10.dp)
+                .background(Color.Yellow))
+        //Space
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = "안녕하세요2")
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(text = "안녕하세요3")
+    }
+}
+
+@Composable
+fun MyComposaableView(){
+    Log.d("TAG", "MyComposaableView : ")
+
+    //Like Vertical LinearLayout
+    Column(
+        Modifier
+            //Column 배경색
+            .background(Color.Green)
+            //세로 스크롤 가능하게 해주기
+            .verticalScroll(rememberScrollState())
+    ) {
+        //아이템 0~31 만들어주기
+        for ( i in 0..30){
+            MyRowItem()
+        }
+    }
+
+}
+
+//미리보기
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
